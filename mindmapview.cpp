08f2025ad52c293e8568aaf6db7706ac318b24e5
Sandeep Datta <datta.sandeep@gmail.com>
2017-02-10 19:57:17 +0530

    Removed some commented code.

--- mindmapview.cpp
+++ mindmapview.cpp
@@ -10,20 +10,6 @@ MindMapView::MindMapView(QQuickItem *parent)
 
 void MindMapView::paint(QPainter *painter)
 {
-//    QPainterPath path;
-//    path.addRect(20, 20, 60, 60);
-
-//    path.moveTo(0, 0);
-//    path.cubicTo(99, 0,  50, 50,  99, 99);
-//    path.cubicTo(0, 99,  50, 50,  0, 0);
-
-//    painter->fillRect(contentsBoundingRect(), Qt::white);
-
-//    painter->setPen(QPen(QColor(79, 106, 25), 1, Qt::SolidLine,
-//                        Qt::FlatCap, Qt::MiterJoin));
-//    painter->setBrush(QColor(122, 163, 39));
-
-//    painter->drawPath(path);
     QRectF paintableRect = contentsBoundingRect();
 
     if (!m_root) {
